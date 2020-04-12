d8fbdcd958e54250e24afe7bf4412a31709361e1
lutungp <lntngp19@gmail.com>
2020-04-12 16:39:32 +0700

    update aja

--- lib/get_started.dart
+++ lib/get_started.dart
@@ -41,46 +41,32 @@ class GetStarted extends StatelessWidget {
             ),
           
             Padding(
-              //SET PADDING UNTUK MENENTUKAN BERAPA JARAK DARI OBJEK SEKITAR
               padding: const EdgeInsets.only(left: 10, right: 10, top: 30),
               
               child: RaisedButton(
                 onPressed: () {
-                  //FUNGSI DARI CODE DIBAWAH ADALAH UNTUK BERPINDAH KE HALAMAN DENGAN ROUTE /LOGIN
-                  //pushReplacementNamed BERARTI KETIKA BERPINDAH SCREEN, MAKA SCREEN SEBELUMNYA AKAN DIHAPUS SEHINGGA USER TIDAK MEMILIKI KEMAMPUAN UNTUK KEMBALI KE HALAMAN SEBELUMNYA
-                
                   Navigator.pushReplacementNamed(context, '/login');
-                
-                  //JANGAN LUPA UNCOMMENT DI ATAS APABILA HALAMAN LOGIN SUDAH DIBUAT
                 },
                   
-                //MODIFIKASI BUTTONNYA MENJADI ROUNDED
                 shape: RoundedRectangleBorder(
-                  //KEMUDIAN BORDERNYA DIBUAT MELENGKUNG DENGAN BORDER RADIUS
                   borderRadius: BorderRadius.circular(80.0),
                 ),
                 padding: const EdgeInsets.all(0.0),
                 //
                 child: Container(
-                  //MEMBUAT DEKORASI GRADIENT DENGAN CONTAINER
                   decoration: const BoxDecoration(
-                    //GRADIENT INI BISA MENAMPUNG BANYAK WARNA UNTUK DIKOMBINASIKAN
                     gradient: LinearGradient( 
-                      //SILAHKAN MASUKKAN CODE WARNANYA, JADI FORMATNYA JIKA INGIN MENGGUNAKAN HEXA COLOR ADALAH 0XFF DITAMBAHAN CODE HEXA, CONTOH HITAM: 0XFF000000 DIMANA 6 ANGKA 0 DIBELAKANG ADALAH HEXA COLOR UNTUK HITAM
                       colors: <Color>[Color(0xFF13E3D2), Color(0xFF5D74E2)],
                     ),
-                    //DARI CONTAINER INI KITA SET BORDER RADIUSNYA DIMANA SETIAP SUDUTNYA MELENGKUNG SEBESAR 80
                     borderRadius: BorderRadius.all(
                       Radius.circular(80.0),
                     ),
                   ),
-                  //KEMUDIAN TENTUKAN UKURANNYA AGAR MEMENUHI BUTTON
                   constraints: const BoxConstraints(
-                    minWidth: 150.0, //SEBENARNYA WIDTH TIDAK PERLU DISET KARENA OTOMATIS MENGIKUTI 
-                    minHeight: 36.0, //INI ADALAH UKURAN DEFAULT DARI RAISED BUTTON
+                    minWidth: 150.0,
+                    minHeight: 36.0,
                   ),
                   alignment: Alignment.center,
-                  //SELANJUTNYA MASUKKAN TEKS UNTUK TOMBOL TERSEBUT
                   child: const Text(
                     'Get Started',
                     textAlign: TextAlign.center,
@@ -93,11 +79,10 @@ class GetStarted extends StatelessWidget {
               ),
             ),
           
-            Spacer(), //DIGUNAKAN UNTUK MEMBUAT JARAK DENGAN WIDGET LAIN
+            Spacer(),
             Container(
-              width: double.infinity, //LEBAR CONTAINERNYA DIBUAT SELEBAR MUNGKIN
+              width: double.infinity,
               decoration: BoxDecoration(
-                //KEMUDIAN DIBUAT MELENGKUNG PADA SUDUT ATAS KANAN-KIRI
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(30),
                   topRight: Radius.circular(30),
--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      //DEFINISIKAN ROUTENYA
       routes: {
-        // ROUTE / BERARTI SECARA OTOMATIS AKAN DI-LOAD KETIKA FLUTTER DIJALANKAN
-        //MAKA KITA MASUKKAN GetStarted()
         '/': (context) => GetStarted(),
-        //ROUTE DENGAN NAME /LOGIN ADALAH HALAMAN SELANJUTNYA
-        //JADI JIKA KITA INGIN MEMANGGIL HALAMAN INI, MAKA GUNAKAN NAME /LOGIN
         '/login': (context) => Login()
       },
     );
